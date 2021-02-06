977fa9e42b942d8b9cc2d95a80d4e85707d7b325
Arda Fakılı <arda.fakili@trendyol.com>
2021-02-06 17:49:00 +0300

    Created spring-boot application

---
+++ src/main/java/com/yaf/huecontroller/HueControllerApplication.java
@@ -0,0 +1,13 @@
+package com.yaf.huecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HueControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HueControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaf/huecontroller/HueControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaf.huecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HueControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
